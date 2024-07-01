2302698b6faf412b1ce989c0d56fd5262bad4fad
Aliaksandr Varachkin <a.varachkin@vendorobotics.com>
2024-07-01 08:20:37 +0200

    fix: fixed open logic

--- src/components/Client/Box.jsx
+++ src/components/Client/Box.jsx
@@ -2,15 +2,24 @@ import {useState} from "react";
 
 export const Box = ({children}) => {
     const [isOpen, setIsOpen] = useState(false)
+    const [isError, setIsError] = useState(false)
 
     const handleOpen = () => {
-        setIsOpen(prev => !prev)
+        const random = Math.round(Math.random())
+        if(random){
+            setIsOpen(prev => !prev)
+        }else if(!isOpen){
+            setIsError(true)
+        }else{
+            setIsError(false)
+        }
+        
     }
 
     return (
         <div className="backDoor">
             <div
-                className={`door ${isOpen ? 'doorOpen' : ''}`}
+                className={`door ${isOpen && !isError? 'doorOpen' : ''} ${isError ? 'doorClosed' : ''}`}
                 onClick={handleOpen}
             >
                 {children}
